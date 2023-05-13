3d1cd67916bcfb8526af18320cdb7ddc90e69e4a
habib-N19 <habiburnabiarafat@gmail.com>
2023-05-13 12:30:42 +0600

    added a state to manage the deleted option to remove without reload

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import { useLoaderData } from 'react-router-dom'
 import CoffeeCard from './components/CoffeeCard'
 
 function App () {
-  const [count, setCount] = useState(0)
-  const coffees = useLoaderData()
+  const loadedCoffees = useLoaderData()
+  const [coffees, setCoffees] = useState(loadedCoffees)
   return (
     <div className='m-20'>
       <h2 className='text-6xl my-20 text-purple-600 text-center'>
@@ -15,7 +15,12 @@ function App () {
       </h2>
       <div className='grid md:grid-cols-2 gap-4'>
         {coffees.map(coffee => (
-          <CoffeeCard key={coffee._id} coffee={coffee}></CoffeeCard>
+          <CoffeeCard
+            key={coffee._id}
+            coffee={coffee}
+            coffees={coffees}
+            setCoffees={setCoffees}
+          ></CoffeeCard>
         ))}
       </div>
     </div>
--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -35,7 +35,7 @@ const AddCoffee = () => {
         if (data.insertedId) {
           Swal.fire({
             title: 'Success!',
-            text: 'User added successfully',
+            text: 'Coffee added successfully',
             icon: 'success',
             confirmButtonText: 'Okay'
           })
